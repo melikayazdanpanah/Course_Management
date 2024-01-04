8d1310eab8944b5e0a9cb67e5c6b946c57e2e81d
melikayazdanpanah <melika.bts7@gmail.com>
2024-01-04 11:36:46 -0800

    def connection

---
+++ app.py
@@ -0,0 +1,12 @@
+import mysql.connector
+from mysql.connector import Error
+
+def connection(database_name):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="melika1380",
+        database= f"{course_management}"
+        )
+    
+    
