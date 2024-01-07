c163965abe8db4884351f99131f70f42212c8bfa
melikayazdanpanah <melika.bts7@gmail.com>
2024-01-07 13:24:57 -0800

    create defs

--- app.py
+++ app.py
@@ -11,4 +11,35 @@ def connection(database_name):
         database= f"{course_management}"
         )
     
+    return connection
+
+
+#create close def 
+
+def close_connection(connection):
+    try:
+        connection.close()
+    except Error as e :
+        print(f"the error '{e}' not close ")
+
+
+    connection.commit()
+#def for creating database 
+
+def create_database(database_name):
+    connection = mysql.connector.connect(
+        host="localhost",
+        user = "root",
+        password = "melika1380",
+    )
+    sql = f"create database {database_name}"
+
+    try_execute(connection=connection,sql_query=sql)
+    close_connection(connection)
+
+
+
+    )
+
+           
     
